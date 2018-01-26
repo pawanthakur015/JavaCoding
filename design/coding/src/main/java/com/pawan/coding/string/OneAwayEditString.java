3e72533f07545e62c5825727e3472fbe1426ce1a
pawankumarthakur <pawan.thakur@spire2grow.com>
2018-01-27 00:11:49 +0530

    -- Added string coding for to check two string difference is at most 1,
    using replace, insert and delete operation.

--- design/coding/src/main/java/com/pawan/coding/string/OneAwayEditString.java
+++ design/coding/src/main/java/com/pawan/coding/string/OneAwayEditString.java
@@ -11,17 +11,22 @@ import com.pawan.coding.util.StringHelper;
  * @author pawankumarthakur
  *
  */
-public class PalindromePermute {
+public class OneAwayEditString {
 
+	/**
+	 * @param args
+	 */
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		
 		StringHelper stringHelper = new StringHelper();
 		Scanner sc = new Scanner(System.in);
-		String Str = sc.next();
-		if(stringHelper.isPalindromicPer(Str)) {
-			System.out.println("palindromic permutation");
+		String str1 = sc.next();
+		String str2 = sc.next();
+		if(stringHelper.oneEditAway(str1, str2)) {
+			System.out.println("---These two string are zero or one edit away---");
 		} else {
-			System.out.println("Not palindromic permutation");
+			System.out.println("---These two string are more than two edit away---");
 		}
 	}
 
--- design/coding/src/main/java/com/pawan/coding/util/StringHelper.java
+++ design/coding/src/main/java/com/pawan/coding/util/StringHelper.java
@@ -9,7 +9,7 @@ package com.pawan.coding.util;
  */
 public class StringHelper {
 
-	private static int getCharNumber(char ch) {
+	private  int getCharNumber(char ch) {
 		int a = Character.getNumericValue('a');
 		int b = Character.getNumericValue('z');
 		int val = Character.getNumericValue(ch);
@@ -21,7 +21,7 @@ public class StringHelper {
 		return -1;
 	}
 	
-	public static boolean isPalindromicPer(String str) {
+	public  boolean isPalindromicPer(String str) {
 		int []freq = new int [(Character.getNumericValue('z') - Character.getNumericValue('a'))+1];
 		int oddValue=0;
 		for(char ch : str.toCharArray()) {
@@ -37,4 +37,45 @@ public class StringHelper {
 		}
 		return oddValue <=1;
 	}
+	
+   public  boolean oneEditAway(String str1, String str2) {
+	   if(str1.length() == str2.length()) {
+		   return oneEditReplace(str1, str2);
+	   } else if(str1.length()-1 == str2.length()) {
+		   return oneEditInsert(str2, str1);
+	   } else if(str1.length() == str2.length()-1) {
+		   return oneEditInsert(str1, str2);
+	   }
+	return false;
+   }
+   
+   private boolean oneEditReplace(String str1, String str2) {
+	   boolean findDiff = false;
+	   for(int i=0;i<str1.length();i++) {
+		   if(str1.charAt(i)!=str2.charAt(i)) {
+			   if(findDiff) {
+				   return false;
+			   }
+			   findDiff = true;
+		   }
+	   }
+	   return true;
+   }
+   
+   private boolean oneEditInsert(String str1, String str2) {
+	   int index1 = 0;
+	   int index2 = 0;
+	   while(index1 < str1.length() && index2 < str2.length()) {
+		   if(str1.charAt(index1)!= str2.charAt(index2)) {
+			   if(index1 != index2) {
+				   return false;
+			   }
+			   index2++;
+		   } else {
+			   index1++;
+			   index2++;
+		   }
+	   }
+	   return true;
+   }
 }
